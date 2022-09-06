bb33df81d89b7baf6301f79eaa28b067e2495d07
Jessir <florezhamburgerjessir@gmail.com>
2022-09-06 11:58:27 -0500

    Arreglos generacion, resueltos

--- Lab.py
+++ Lab.py
@@ -21,11 +21,11 @@ if opcion == "Seno":
 
     st.title("Función Seno")
 
-    frecuencia = st.number_input("Por favor ingrese el valor de la frecuencia f: ",step=0.1,min_value=0.1)
-    amplitude = st.number_input("Por favor ingrese el valor de la amplitud A: ")
+    frecuencia = st.number_input("Por favor ingrese el valor de la frecuencia f: ",step=1,min_value=1,max_value=10)
+    amplitude = st.number_input("Por favor ingrese el valor de la amplitud A: ",step=1,min_value=-10,max_value=10)
     
     paso=(1/(300*frecuencia))
-    x=np.arange(0,2/frecuencia,paso)
+    x=np.arange(0,2*np.pi,paso)
     y=np.sin(2*np.pi*frecuencia*x)
     y=y*amplitude
 
@@ -34,20 +34,22 @@ if opcion == "Seno":
     ax.set_title("Función Seno")
     ax.set_xlabel("Eje x")
     ax.set_ylabel("Eje y")
+    ax.set_xlim(0,np.pi)
+    ax.set_ylim(-10,10)
     ax.grid(True)
     st.pyplot(fig)
 
 #FUNCION PULSO
-if opcion == "Pulso":
+elif opcion == "Pulso":
 
     st.title("Función Pulso")
 
-    amplitud = st.number_input("Por favor ingrese el valor de la amplitud del pulso: ")
-    anchura = st.number_input("Por favor ingrese el valor de la anchura del pulso: ")
+    amplitud = st.number_input("Por favor ingrese el valor de la amplitud del pulso: ",step=1,min_value=0,max_value=10)
+    anchura = st.number_input("Por favor ingrese el valor de la anchura del pulso: ",step=1,min_value=1,max_value=10)
     
     visualize=anchura/2
     percentage=visualize*0.5
-    x = np.arange((-visualize)-percentage,(visualize)+percentage,.02)
+    x = np.arange((-visualize)-percentage,(visualize)+percentage,.002)
     x_rect = ss.rect(x,anchura)
     y=x_rect*amplitud
 
@@ -56,21 +58,23 @@ if opcion == "Pulso":
     ax.set_title("Función pulso")
     ax.set_xlabel("Eje x")
     ax.set_ylabel("Eje y")
+    ax.set_xlim(-6,6)
+    ax.set_ylim(0,10)
     ax.grid(True)
     st.pyplot(fig)
 
 #FUNCION CUADRATICA
-if opcion == "Cuadratica":
+elif opcion == "Cuadratica":
 
     st.title("Función Cuadrática")
-    st.subheader("y(t)= At^2 + bt + c.")
+    st.latex("ax^2+2bx+c")
 
-    A=st.number_input("Ingrese el valor de la constante A: ")
-    B=st.number_input("Ingrese el valor de la constante B: ")
-    C=st.number_input("Ingrese el valor de la cosntante C: ")
+    val2=st.number_input("Ingrese el valor de la constante A: ",step=1,min_value=0,max_value=10)
+    B=st.number_input("Ingrese el valor de la constante B: ",step=1,min_value=0,max_value=10)
+    C=st.number_input("Ingrese el valor de la cosntante C: ",step=1,min_value=0,max_value=10)
 
     x = np.arange(-10,10,.01)
-    y = A*x**2+B*x+C
+    y = val2*x**2+B*x+C
         
     
     fig,ax=plt.subplots()
@@ -82,15 +86,16 @@ if opcion == "Cuadratica":
     st.pyplot(fig)
 
 #FUNCION EXPONENCIAL
-if opcion == "Exponencial":
+elif opcion == "Exponencial":
     
     st.title("Funcion Exponencial")
+    st.latex("y(t)=Ae^{-bt}")
 
-    A=st.number_input("Ingrese el valor de la constante A: ")
-    b=st.number_input("Ingrese el valor de la constante b: ")
+    val2=st.number_input("Ingrese el valor de la constante A: ",step=1,min_value=1,max_value=10)
+    b=st.number_input("Ingrese el valor de la constante b: ",step=1,min_value=-5,max_value=10)
     x= np.arange(0,2*np.pi,0.01)
     
-    y = A*np.exp(-b*x)
+    y = val2*np.exp(-b*x)
 
     fig,ax=plt.subplots()
     ax.plot(x,y)
@@ -101,15 +106,15 @@ if opcion == "Exponencial":
     st.pyplot(fig)
     
 #FUNCION LINEAL
-if opcion=="Lineal":
+elif opcion=="Lineal":
 
     st.title("Función Lineal")
-    st.subheader("y=mx+b")
+    st.latex("y=mx+b")
 
-    m=st.number_input("Ingrese el valor de la constante m: ")
-    b=st.number_input("Ingrese el valor de la cosntante b: ")
+    m=st.number_input("Ingrese el valor de la constante m: ",step=1,min_value=-10,max_value=10)
+    b=st.number_input("Ingrese el valor de la constante b: ",step=1,min_value=-10,max_value=10)
 
-    x = np.arange(0, 10, 0.01)
+    x = np.arange(-10,10, 0.01)
     y = (m*x)+b
 
     fig,ax=plt.subplots()
@@ -117,19 +122,21 @@ if opcion=="Lineal":
     ax.set_title("Función lineal")
     ax.set_xlabel("Eje x")
     ax.set_ylabel("Eje y")
+    ax.set_xlim(-10,10)
+    ax.set_ylim(-50,50)
     ax.grid(True)
     st.pyplot(fig)
 
 #FUNCION TRIANGULAR
-if opcion == "Triangular":
+elif opcion == "Triangular":
    
     st.title('Funcion Triangular')
 
-    a = st.number_input('Ingrese a continuacion el valor de la amplitud:')
-    f = st.number_input('Ingrese a continuación el valor de la frecuencia:',step=0.1,min_value=0.1)
+    a = st.number_input('Ingrese a continuacion el valor de la amplitud:',step=1,min_value=1,max_value=10)
+    f = st.number_input('Ingrese a continuación el valor de la frecuencia:',step=1,min_value=1,max_value=10)
 
-    paso=(1/(300*f))
-    x = np.arange(0,2/f,paso)
+    paso=(1/(10000*f))
+    x = np.arange(0,np.pi/2,paso)
     y = a*signal.sawtooth(2 *np.pi*f*x)
     
     fig,ax=plt.subplots()
@@ -137,19 +144,21 @@ if opcion == "Triangular":
     ax.set_title("Función triangular")
     ax.set_xlabel("Eje x")
     ax.set_ylabel("Eje y")
+    ax.set_xlim(0,np.pi/2)
+    ax.set_ylim(-11,11)
     ax.grid(True)
     st.pyplot(fig)
 
 #FUNCION CUADRADA
-if opcion == "Cuadrada":
+elif opcion == "Cuadrada":
    
     st.title('Funcion Cuadrada')
 
-    a = st.number_input('Ingrese a continuacion el valor de la amplitud:')
-    f = st.number_input('Ingrese a continuación el valor de la frecuencia:',step=0.1,min_value=0.1)
+    a = st.number_input('Ingrese a continuacion el valor de la amplitud:',step=1,min_value=1,max_value=10)
+    f = st.number_input('Ingrese a continuación el valor de la frecuencia:',step=1,min_value=1,max_value=10)
 
     paso=(1/(300*f))
-    x = np.arange(0,2/f,paso)
+    x = np.arange(0,np.pi/2,paso)
     y = a*signal.square(2*np.pi*f*x)
     
     fig,ax=plt.subplots()
@@ -157,11 +166,13 @@ if opcion == "Cuadrada":
     ax.set_title("Función cuadrada")
     ax.set_xlabel("Eje x")
     ax.set_ylabel("Eje y")
+    ax.set_xlim(0,np.pi/2)
+    ax.set_ylim(-11,11)
     ax.grid(True)
     st.pyplot(fig)
     
 #SECUENCIA DE IMPULSOS
-if opcion == "Secuencia de impulsos":
+elif opcion == "Secuencia de impulsos":
    
     st.title('Secuencia de impulsos')
 
@@ -197,8 +208,8 @@ if "Desplazamiento" in transformaciones:
     val = st.sidebar.number_input("",step=1,min_value=-5,max_value=5)
     
     traslado=(x+val)
-    fig,ax=plt.subplots()
-    for i in range(frames+1):        
+    for i in range(frames+1):   
+        fig,ax=plt.subplots()
         ax.plot(x,y)
         ax.plot(x+i*(val)/frames,y,linestyle="dashed")
         ax.legend(['Original', 'Desplazada'])
@@ -207,3 +218,19 @@ if "Desplazamiento" in transformaciones:
         ax.set_ylabel("Eje y")
         ax.grid(True)
         st.pyplot(fig)
+        
+        if "Escalamiento en tiempo" in transformaciones:
+            st.sidebar.subheader('Escoja a continuación cuantas unidades desea escalar: ')
+            val2 = st.sidebar.number_input("",step=1,min_value=-3,max_value=3)
+            
+            traslado=(x*(1/val2))
+            for i in range(frames+1):   
+                fig,ax=plt.subplots()
+                ax.plot(x,y)
+                ax.plot(x,y*(1 + i*(val2-1)/frames),linestyle="dashed")
+                ax.legend(['Original', 'Desplazada'])
+                ax.set_title("Función Seno")
+                ax.set_xlabel("Eje x")
+                ax.set_ylabel("Eje y")
+                ax.grid(True)
+                st.pyplot(fig)
