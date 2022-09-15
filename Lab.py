ce3516f29b0c520f0cfeb2f535a38d1d77dea9cc
Jessir <florezhamburgerjessir@gmail.com>
2022-09-15 11:06:04 -0500

    Versión finalizada

--- Lab.py
+++ Lab.py
@@ -23,7 +23,7 @@ if opcion == "Seno":
     st.title("Función Seno")
 
     frecuencia = st.number_input("Por favor ingrese el valor de la frecuencia f: ",step=1,min_value=1,max_value=10)
-    amplitude = st.number_input("Por favor ingrese el valor de la amplitud A: ",step=1,min_value=-10,max_value=10)
+    amplitude = st.number_input("Por favor ingrese el valor de la amplitud A: ",step=1,min_value=-10,max_value=10,value=1)
     
     paso=(1/(300*frecuencia))
     x=np.arange(0,2*np.pi,paso)
@@ -45,8 +45,8 @@ elif opcion == "Pulso":
 
     st.title("Función Pulso")
 
-    amplitud = st.number_input("Por favor ingrese el valor de la amplitud del pulso: ",step=1,min_value=0,max_value=10)
-    anchura = st.number_input("Por favor ingrese el valor de la anchura del pulso: ",step=1,min_value=1,max_value=10)
+    amplitud = st.number_input("Por favor ingrese el valor de la amplitud del pulso: ",step=1,min_value=0,max_value=10,value=1)
+    anchura = st.number_input("Por favor ingrese el valor de la anchura del pulso: ",step=1,min_value=1,max_value=10,value=1)
     
     visualize=anchura/2
     percentage=visualize*0.5
@@ -70,7 +70,7 @@ elif opcion == "Cuadratica":
     st.title("Función Cuadrática")
     st.latex("ax^2+2bx+c")
 
-    Escal=st.number_input("Ingrese el valor de la constante A: ",step=1,min_value=0,max_value=10)
+    Escal=st.number_input("Ingrese el valor de la constante A: ",step=1,min_value=0,max_value=10,value=1)
     B=st.number_input("Ingrese el valor de la constante B: ",step=1,min_value=0,max_value=10)
     C=st.number_input("Ingrese el valor de la cosntante C: ",step=1,min_value=0,max_value=10)
 
@@ -92,8 +92,8 @@ elif opcion == "Exponencial":
     st.title("Funcion Exponencial")
     st.latex("y(t)=Ae^{-bt}")
 
-    Escal=st.number_input("Ingrese el valor de la constante A: ",step=1,min_value=1,max_value=10)
-    b=st.number_input("Ingrese el valor de la constante b: ",step=1,min_value=-5,max_value=10)
+    Escal=st.number_input("Ingrese el valor de la constante A: ",step=1,min_value=1,max_value=10,value=1)
+    b=st.number_input("Ingrese el valor de la constante b: ",step=1,min_value=-5,max_value=10,value=1)
     x= np.arange(0,2*np.pi,0.01)
     
     y = Escal*np.exp(-b*x)
@@ -112,8 +112,8 @@ elif opcion=="Lineal":
     st.title("Función Lineal")
     st.latex("y=mx+b")
 
-    m=st.number_input("Ingrese el valor de la constante m: ",step=1,min_value=-10,max_value=10)
-    b=st.number_input("Ingrese el valor de la constante b: ",step=1,min_value=-10,max_value=10)
+    m=st.number_input("Ingrese el valor de la constante m: ",step=1,min_value=-10,max_value=10,value=1)
+    b=st.number_input("Ingrese el valor de la constante b: ",step=1,min_value=-10,max_value=10,value=1)
 
     x = np.arange(-10,10, 0.01)
     y = (m*x)+b
@@ -133,8 +133,8 @@ elif opcion == "Triangular":
    
     st.title('Funcion Triangular')
 
-    a = st.number_input('Ingrese a continuacion el valor de la amplitud:',step=1,min_value=1,max_value=10)
-    f = st.number_input('Ingrese a continuación el valor de la frecuencia:',step=1,min_value=1,max_value=10)
+    a = st.number_input('Ingrese a continuacion el valor de la amplitud:',step=1,min_value=1,max_value=10,value=1)
+    f = st.number_input('Ingrese a continuación el valor de la frecuencia:',step=1,min_value=1,max_value=10,value=3)
 
     paso=(1/(10000*f))
     x = np.arange(0,np.pi/2,paso)
@@ -156,7 +156,7 @@ elif opcion == "Cuadrada":
     st.title('Funcion Cuadrada')
 
     a = st.number_input('Ingrese a continuacion el valor de la amplitud:',step=1,min_value=1,max_value=10)
-    f = st.number_input('Ingrese a continuación el valor de la frecuencia:',step=1,min_value=1,max_value=10)
+    f = st.number_input('Ingrese a continuación el valor de la frecuencia:',step=1,min_value=1,max_value=10,value=3)
 
     paso=(1/(300*f))
     x = np.arange(0,np.pi/2,paso)
@@ -173,88 +173,125 @@ elif opcion == "Cuadrada":
     st.pyplot(fig)
     
 #SECUENCIA DE IMPULSOS
-elif opcion == "Secuencia de impulsos":
-   
-    st.title('Secuencia de impulsos')
 
-    x = st.text_input("Ingrese el valor de los vectores de x: ",args=None)
-    y = st.text_input("Ingrese el valor de los vectores de y: ",args=None)
+if opcion=="Secuencia de impulsos":
+    x = st.sidebar.text_input('x []:')
+    y = st.sidebar.text_input('y []:')
 
-    
-    
-    if len(x) != len(y):
-        st.write("Ingrese vectores equivalentes")
+    x = np.array(np.matrix(x)).ravel()
+    y = np.array(np.matrix(y)).ravel()
 
+
+    if len(x)!=len(y):
+        st.error('Debe ingresar longitudes iguales para que pueda graficarse correctamente')
+            
     else:
-        fig,ax=plt.subplots()
-        ax.set_xlim(-1,11)
-        ax.set_ylim(0,10)
+        fig, ax = plt.subplots()
         ax.stem(x,y)
-        ax.set_title("Función secuencia de impulsos")
+        ax.set_title("Secuencia de impulsos")
         ax.set_xlabel("Eje x")
-        ax.set_ylabel("Eje y")
+        ax.set_ylabel("Eje Y")
+        ax.set_xlim()
         ax.grid(True)
         st.pyplot(fig)
 
-
+        
+#ETAPA DE TRANSFORMACION DE SEÑALES
 st.sidebar.subheader('Etapa 2 - Transformación de señales')
+st.sidebar.subheader("Etapa de transformaciones")
 
-transformaciones = st.sidebar.multiselect(
-     'Escoja las transformaciones que desea realizar a continuación: ',
-     ['Desplazamiento', 'Escalamiento en tiempo', 'Escalamiento en amplitud'])
-
-frames=10
+#DECLARACION DE VALORES PARA GRAFICAR
+frames=15
 graficar=st.empty()
 
-if "Desplazamiento" in transformaciones:
-    st.sidebar.subheader('Escoja a continuación cuantas unidades desea desplazar: ')
-    num = st.sidebar.number_input("",step=1,min_value=-5,max_value=5)
-    traslado=(x+num)
-    
-    for i in range(frames+1):   
-        fig,ax=plt.subplots()
-        ax.plot(x,y)
-        ax.plot(x+i*(num)/frames,y,linestyle="dashed")
-        ax.legend(['Original', 'Desplazada'])
-        ax.set_title("Función Seno")
-        ax.set_xlabel("Eje x")
-        ax.set_ylabel("Eje y")
-        ax.grid(True)
-        graficar.pyplot(fig)
-        
-    if "Escalamiento en amplitud" in transformaciones:
-        st.sidebar.subheader('Escoja a continuación cuantas unidades desea escalar: ')
-        Escal= st.sidebar.number_input("",step=1,min_value=-3,max_value=3)
-            
-        Escal=(y*Escal)
+#DESPLAZAMIENTO EN EL TIEMPO
+Desplazamiento = st.sidebar.number_input("Ingrese cuantas unidades desea desplazar: ", step=1,min_value=-5,max_value=5,value=0)
+new_x=x-Desplazamiento
 
+#ESCALAMIENTO EN AMPLITUD
+Escalamiento_a = st.sidebar.number_input("Ingrese cuantas unidades desea escalar en amplitud: ", step=0.1,min_value=0.0,max_value=10.0,value=1.0)
+new_y=y*Escalamiento_a
+
+#ESCALAMIENTO EN EL TIEMPO
+Escal_time = st.sidebar.number_input("Ingrese cuantas unidades desea escalar en el tiempo: ", step=0.1,min_value=0.0,max_value=5.0,value=1.0)
+animacion = st.sidebar.button('Animación')
+
+
+#GRAFICAR TRANSFORMACIONES
+if animacion:
+
+#GRAFICA DE DESPLAZAMIENTO
+    if opcion=="Secuencia de impulsos":
+        for i in range(frames+1):   
+            fig,ax=plt.subplots()
+            ax.stem(x,y)
+            ax.stem(x-i*(Desplazamiento)/frames,y,linefmt ="rx")
+            ax.legend(['Original', 'Transformada'])
+            ax.set_title("Transformaciones")
+            ax.set_xlabel("Eje x")
+            ax.set_ylabel("Eje y")
+            ax.grid(True)
+            graficar.pyplot(fig)
+            
+    else:
         for i in range(frames+1):   
             fig,ax=plt.subplots()
             ax.plot(x,y)
-            ax.plot(traslado, y*(1 + i*(Escal-1)/frames),linestyle="dashed")
-            ax.legend(['Original', 'Desplazada'])
-            ax.set_title("Función Seno")
+            ax.plot(x-i*(Desplazamiento)/frames,y,linestyle="dashed")
+            ax.legend(['Original', 'Transformada'])
+            ax.set_title("Transformaciones")
             ax.set_xlabel("Eje x")
             ax.set_ylabel("Eje y")
             ax.grid(True)
             graficar.pyplot(fig)
 
 
-        if "Escalamiento en tiempo" in transformaciones:
-            st.sidebar.subheader('Escoja a continuación cuantas unidades desea escalar en el tiempo: ')
-            Escal_time= st.sidebar.number_input("",step=1,min_value=-4,max_value=4)
-                
-            Escal_time=(x*Escal_time)
+#GRAFICA DE ESCALAMIENTO EN AMPLITUD
+    if opcion=="Secuencia de impulsos":
+        for i in range(frames+1):   
+            fig,ax=plt.subplots()
+            ax.stem(x,y)
+            ax.stem(new_x,y*(1 + i*(Escalamiento_a-1)/frames),linefmt="rx")
+            ax.legend(['Original', 'Transformada'])
+            ax.set_title("Transformaciones")
+            ax.set_xlabel("Eje x")
+            ax.set_ylabel("Eje y")
+            ax.grid(True)
+            graficar.pyplot(fig)
 
-            for i in range(frames+1):   
-                fig,ax=plt.subplots()
-                ax.plot(x,y)
-                ax.plot(traslado, y*(1 + i*(Escal_time-1)/frames),linestyle="dashed")
-                ax.legend(['Original', 'Desplazada'])
-                ax.set_title("Función Seno")
-                ax.set_xlabel("Eje x")
-                ax.set_ylabel("Eje y")
-                ax.grid(True)
-                graficar.pyplot(fig)
+    else:
+        for i in range(frames+1):   
+            fig,ax=plt.subplots()
+            ax.plot(x,y)
+            ax.plot(new_x,y*(1 + i*(Escalamiento_a-1)/frames),linestyle="dashed")
+            ax.legend(['Original', 'Transformada'])
+            ax.set_title("Transformaciones")
+            ax.set_xlabel("Eje x")
+            ax.set_ylabel("Eje y")
+            ax.grid(True)
+            graficar.pyplot(fig)
 
-    
+#GRAFICA DE ESCALAMIENTO EN EL TIEMPO
+    if opcion=="Secuencia de impulsos":
+        for i in range(frames+1):   
+            fig,ax=plt.subplots()
+            ax.stem(x,y)
+            ax.stem((new_x/(1+ i*(Escal_time-1)/frames)),new_y,linefmt="rx")
+            ax.legend(['Original', 'Transformada'])
+            ax.set_title("Transformaciones")
+            ax.set_xlabel("Eje x")
+            ax.set_ylabel("Eje y")
+            ax.grid(True)
+            graficar.pyplot(fig)
+
+    else:
+        for i in range(frames+1):   
+            fig,ax=plt.subplots()
+            ax.plot(x,y)
+            ax.plot((new_x/(1+ i*(Escal_time-1)/frames)),new_y,linestyle="dashed")
+            ax.legend(['Original', 'Transformada'])
+            ax.set_title("Transformaciones")
+            ax.set_xlabel("Eje x")
+            ax.set_ylabel("Eje y")
+            ax.grid(True)
+            graficar.pyplot(fig)
